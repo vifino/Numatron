504861c6709bb3d3ad40ad0d5ea6fa6a402c4242
vifino <adinator1999@gmail.com>
2014-07-20 18:29:25 +0200

    Fixed some things, should be a bit more resource friendly now

--- modules/lua.rb
+++ modules/lua.rb
@@ -2,19 +2,16 @@ require 'rufus-lua'
 @luastate = Rufus::Lua::State.new
 def lua(args,nick,chan)
 	if (isPrivileged nick) and args != nil then
-		job_lua = fork do
-			begin
-				returnval = @luastate.eval(args)
-				if returnval!=nil then
-					#@bot.msg(chan,"> "+ returnval.inspect)
-					return returnval.inspect
-				end
-			rescue => detail
-				#@bot.msg(chan,detail.message())
-				return detail.message
+		begin
+			returnval = @luastate.eval(args)
+			if returnval!=nil then
+				#@bot.msg(chan,"> "+ returnval.inspect)
+				return returnval.inspect
 			end
-		end
-		Process.detach(job_lua)
+		rescue => detail
+			#@bot.msg(chan,detail.message())
+			return detail.message
+		end		
 	end
 end
 @commands["lua"] = :lua
