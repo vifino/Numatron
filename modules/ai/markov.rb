2e0ab14dcca354162a856703380a33d2522347cf
vifino <adinator1999@gmail.com>
2014-07-22 22:26:32 +0200

    Added non-working markov chain, do not use

---
+++ modules/ai/markov.rb
@@ -0,0 +1,46 @@
+# Markov Chain, do not use, doesnt work
+# Made by vifino
+class Markov
+	attr_reader :parts
+	def initialize()
+    @parts = Hash.new # Storage for data
+		@startParts = Array.new
+  end
+	def learn(str)
+		input=str.split(" ")
+		@startParts.push(input[0]+" "+input[1])
+		for i in 1 ... input.size do
+			if input[i+1] then
+				part1=input[i]
+				part2=input[i+1]
+				@parts[part1+" "+part2] = (input[1+2] or "")
+			end
+		end
+	end
+	def generate()
+		start = @startParts.sample
+		finished = false
+		out = start
+		until finished do
+			a = out.split(" ")
+			lastPart = a[a.count-1] + " " + a[a.count]
+			part = nextPart(lastPart)
+			if part then
+				a=part.split()
+				if a[1] then
+					out = out+" "+a[1]
+				else
+					return out
+				end
+			else
+				return out
+			end
+		end
+	end
+	def nextPart(part)
+		@parts[part] = @parts[part] or Array.new()
+		part2=@parts[part].sample or ""
+		part1=part.split(" ")[1]
+		return part1+" "+part2
+	end
+end
