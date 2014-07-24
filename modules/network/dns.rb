3562270b83fae0a7870c04a2777b79847aded1c3
vifino <adinator1999@gmail.com>
2014-07-24 14:24:57 +0200

    Updated dns.rb

--- modules/network/dns.rb
+++ modules/network/dns.rb
@@ -1,19 +1,17 @@
 # Perform DNS lookups
 # Made by vifino
 require "dnsruby"
-def dns(args="",nick="",chan="",rawargs="",pipeargs="")
-	addr = args.split.first
+def dns(addr,type="ANY")
 	rs = Dnsruby::DNS.new
 	begin
-		res = rs.getresources(addr, "ANY")
+		res = rs.getresources(addr, type)
 		return "No results!" if res.empty?
 		return (res.map {|r| r.rdata_to_string.gsub(/[[:cntrl:]]/, '') }).join(", ")
 	rescue => e
 		return e.to_s
 	end
 end
-def rdns(args="",nick="",chan="",rawargs="",pipeargs="")
-	addr = args.split.first
+def rdns(addr)
 	rs = Dnsruby::DNS.new
 	begin
 		res = rs.getnames addr
@@ -23,5 +21,19 @@ def rdns(args="",nick="",chan="",rawargs="",pipeargs="")
 		return e.to_s
 	end
 end
-$commands["dns"] = :dns
-$commands["rdns"] = :rdns
+def cmd_dnsall(args,nick,chan,rawargs="",pipeargs="")
+	return dns(args.split.first)
+end
+def cmd_dnsa(args,nick,chan,rawargs="",pipeargs="")
+	return dns(args.split.first,"A")
+end
+def cmd_dnst(args,nick,chan,rawargs="",pipeargs="")
+	return dns(args.split.last,args.split.first)
+end
+def cmd_rdns(args,nick,chan,rawargs="",pipeargs="")
+	return rdns(args.split.first)
+end
+$commands["dns_all"] = :cmd_dnsall
+# $commands["dns_type"] = :cmd_dnst doesnt work :/
+$commands["dns"] = :cmd_dnsa
+$commands["rdns"] = :cmd_rdns
