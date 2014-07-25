ca594b954f42a5f3cfaa546b1ce9923864d20485
vifino <adinator1999@gmail.com>
2014-07-25 12:43:33 -0700

    Added some basic "encryption" things

---
+++ core/crypt.rb
@@ -0,0 +1,17 @@
+# Missleading name. Maybe.
+# Made by vifino
+require "base64"
+def base64(args,nick,chan,rawargs="",pipeargs="")
+	Base64.encode(args)
+end
+def debase64(args,nick,chan,rawargs="",pipeargs="")
+	Base64.decode(args)
+end
+$commands["b64"] :base64
+$commands["base64"] :base64
+$commands["deb64"] :debase64
+$commands["debase64"] :debase64
+def rot13(args,nick,chan,rawargs="",pipeargs="")
+	args.tr 'A-Za-z','N-ZA-Mn-za-m'
+end
+$commands["rot13"] = :rot13
