6e1b4042ab1eeafcca06046b6cc895033aaa8023
vifino <adinator1999@gmail.com>
2014-07-27 01:31:26 +0200

    Changed some things, rainbow doesn't work again.

--- core/stringfuncs.rb
+++ core/stringfuncs.rb
@@ -4,17 +4,16 @@ def cmd_sqeeze(args,nick,chan,rawargs="",pipeargs="")
 	return (args or "").squeeze()
 end
 $commands["sqeeze"] = :cmd_sqeeze
-def cmd_rainbow(args,nick,chan,rawargs="",pipeargs="")
+def cmd_rainbow(args,nick,chan,rawargs="",pipeargs="") # seems broken, I dont know why...
 	if not args.empty? then
 		colors=["04","07","08","03","02","12","06","04","07"] # color is "\u0003<val>""
 		str = ""
 		c=0
-		args.split("").each do|char|
+		args.split("").each {|char|
 			c+=1
 			if c == 8 then c=0 end
-			puts c
   		str+="\u0003"+colors[c]+char+"\u000F"
-		end
+		}
 		return str
 	end
 end
