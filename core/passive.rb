a843985ee9dda98aa486b6193ce551d5d54d38e7
vifino <adinator1999@gmail.com>
2014-07-26 15:58:36 +0200

    Fixed some random junk.

--- core/passive.rb
+++ core/passive.rb
@@ -29,12 +29,14 @@ def passive_process(raw)
 	username = data[4]
 	hostname = data[5]
 	if type=="msg" or type=="join" then
-			if not @passivedata.include? nick then @passivedata[nick] = {} end
-			@passivedata[nick]["user"] = username
-			@passivedata[nick]["host"] = hostname
-			if not @passivedata[nick]["chan"] then @passivedata[nick]["chan"] = [] end
-			if not @passivedata[nick]["chan"].include? chan then @passivedata[nick]["chan"].push chan end
+		if not @passivedata then @passivedata = {} end
+		if not @passivedata.include? nick then @passivedata[nick] = {} end
+		@passivedata[nick]["user"] = username
+		@passivedata[nick]["host"] = hostname
+		if not @passivedata[nick]["chan"] then @passivedata[nick]["chan"] = [] end
+		if not @passivedata[nick]["chan"].include? chan then @passivedata[nick]["chan"].push chan end
 	elsif type=="notice" then
+		if not @passivedata then @passivedata = {} end
 		if nick == "NickServ" then
 			if ret = raw.match(/:Information on (.*?) \(account (.*?)\)/) then
 				puts "Nickserv account info!"
@@ -47,6 +49,7 @@ def passive_process(raw)
 			end
 		end
 	elsif match = raw.match(/^:(.*) 353 (.*?) (.*?) (.*?) :(.*)/)
+		if not @passivedata then @passivedata = {} end
 		chan2 = match[4]
 		mode = match[3]
 		names = match[5]
@@ -57,6 +60,7 @@ def passive_process(raw)
 			if not @passivedata[name]["chan"].include? chan2 then @passivedata[name]["chan"].push chan2 end
 		}
 	elsif match = raw.match(/^:(.*?) 319 (.*?) (.*?) :(.*)/) then
+		if not @passivedata then @passivedata = {} end
 		name = match[3]
 		chans = match[4]
 		chans.split(" ").each {|chan2|
@@ -66,6 +70,7 @@ def passive_process(raw)
 			if not @passivedata[name]["chan"].include? chan2 then @passivedata[name]["chan"].push chan2 end
 		}
 	elsif match = raw.match(/^:(.*?) 311 (.*?) (.*?) (.*?) (.*?) (.*?) :(.*)/) then
+		if not @passivedata then @passivedata = {} end
 		name = match[3]
 		user = match[4]
 		addr = match[5]
@@ -74,26 +79,28 @@ def passive_process(raw)
 		@passivedata[name]["addr"] = addr
 		@passivedata[name]["real"] = real.delete("^\u{0000}-\u{007F}") # Remove unicode, because it kills .to_json
 	elsif match = raw.match(/^:(.*) 354 (.*?) (.*?) (.*?) (.*?) (.*?) (.*?) (.*?) (.*?) (.*?) (.*?):(.*)/) then
-			chan = match[3]
-			user = match[4]
-			ip = match[5]
-			host = match[6]
-			server = match[7]
-			nick = match[8]
-			mode = match[9]
-			acc = match[10]
-			realname = match[12].delete("^\u{0000}-\u{007F}") # Remove unicode, because it kills .to_json
-			if not @passivedata.include? nick then @passivedata[nick] = {} end
-			if not @passivedata[nick]["chan"] then @passivedata[nick]["chan"] = [] end
-			if not @passivedata[nick]["chan"].include? chan then @passivedata[nick]["chan"].push chan end
-			@passivedata[nick]["user"] = user
-			@passivedata[nick]["host"] = host
-			@passivedata[nick]["server"] = server
-			@passivedata[nick]["acc"] = acc
-			@passivedata[nick]["ip"] = ip
-			@passivedata[nick]["real"] = realname
+		if not @passivedata then @passivedata = {} end
+		chan = match[3]
+		user = match[4]
+		ip = match[5]
+		host = match[6]
+		server = match[7]
+		nick = match[8]
+		mode = match[9]
+		acc = match[10]
+		realname = match[12].delete("^\u{0000}-\u{007F}") # Remove unicode, because it kills .to_json
+		if not @passivedata.include? nick then @passivedata[nick] = {} end
+		if not @passivedata[nick]["chan"] then @passivedata[nick]["chan"] = [] end
+		if not @passivedata[nick]["chan"].include? chan then @passivedata[nick]["chan"].push chan end
+		@passivedata[nick]["user"] = user
+		@passivedata[nick]["host"] = host
+		@passivedata[nick]["server"] = server
+		@passivedata[nick]["acc"] = acc
+		@passivedata[nick]["ip"] = ip
+		@passivedata[nick]["real"] = realname
 	elsif type=="nick" then
 		# Move table to new pos
+		if not @passivedata then @passivedata = {} end
 		if not @passivedata.include? nick then @passivedata[nick] = {} end
 		@passivedata[chan] = @passivedata[nick]
 		@passivedata[nick] = nil
