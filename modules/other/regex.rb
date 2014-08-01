95a7bfe475111d5ba3384a23f57d7795f0edd963
vifino <adinator1999@gmail.com>
2014-08-01 11:47:06 +0200

    Added Global substitution to regex.rb

--- modules/other/regex.rb
+++ modules/other/regex.rb
@@ -2,7 +2,9 @@
 # Made by vifino
 def regex_cmd(args="",nick="",chan="",rawargs="",pipeargs="")
 	if not args.empty? then
-		if match=rawargs.match(/s\/(.*)\/(.*)\//) or match=rawargs.match(/s\/(.*)\/(.*)/) then
+		if match=rawargs.match(/s\/(.*)\/(.*)\/g/)then
+			data=pipeargs.gsub(/#{match[1]}/,"#{match[2]}")
+		elsif match=rawargs.match(/s\/(.*)\/(.*)\//) or match=rawargs.match(/s\/(.*)\/(.*)/) then
 			data=pipeargs.sub(/#{match[1]}/,"#{match[2]}")
 		end
 		return data
