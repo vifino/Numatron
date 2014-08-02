2f8cc017986971358365f5fcc8210c0b4115fecb
vifino <adinator1999@gmail.com>
2014-08-02 21:45:19 +0200

    Added.... Things.

--- modules/other/random.rb
+++ modules/other/random.rb
@@ -1,3 +1,11 @@
 # Very random things.
 # Made by vifino
 $commands["totallyinsane"]= lambda {|args="",nick="",chan="",rawargs="",pipeargs=""| return "I agree."}
+$commands["suchunexpected"]= lambda {|args="",nick="",chan="",rawargs="",pipeargs=""| return "I saw that comming!"}
+$commands["blergh"]= lambda {|args="",nick="",chan="",rawargs="",pipeargs=""| return "no u"}
+$commands["didhejust"]= lambda {|args="",nick="",chan="",rawargs="",pipeargs=""| return "Yes. Yes, he did."}
+$commands["nohedidnt"]= lambda {|args="",nick="",chan="",rawargs="",pipeargs=""| return "No, #{nick}. He did."}
+$commands["nohedidn't"]= lambda {|args="",nick="",chan="",rawargs="",pipeargs=""| return "No, #{nick}. He did."}
+$commands["noyoudidnt"]= lambda {|args="",nick="",chan="",rawargs="",pipeargs=""| return "No, I did!"}
+$commands["noyoudidn't"]= lambda {|args="",nick="",chan="",rawargs="",pipeargs=""| return "No, I did!"}
+$commands["lies"]= lambda {|args="",nick="",chan="",rawargs="",pipeargs=""| return "Errr... No?"}
