054ba0909c7526f71601fcebb7b6c93d511bd14c
vifino <adinator1999@gmail.com>
2014-08-04 12:14:59 +0200

    Added variables, access them like <varname>

--- Numatron.rb
+++ Numatron.rb
@@ -35,117 +35,32 @@ def loadSettings(file = "settings.rb")
 		@ssl=false
 	end
 end
-def init_fifos
+def init_fifos # not used anymore...
 	@fifopassive = open("pipes/passive","w+")
 	@fifopassive.flush
 	@fifoauth = open("pipes/auth","w+")
 	@fifoauth.flush
 end
-def sendfifos(raw)
+def sendfifos(raw) # This is not used too.
 	@fifopassive.puts raw
 	@fifopassive.flush
 	@fifoauth.puts raw
 	@fifoauth.flush
 end
-$commands = Hash.new()
 @rawhooks = []
 loadSettings
+$commands ||= Hash.new()
 runDir "core"
 def runRaw(raw)
 	@rawhooks.each {|item|
 	if item.is_a?(Method) then
-		$commands[func].call(raw)
+		item.call(raw)
 	else
 		self.send(item,raw)
 	end
 	}
 end
 # spacer! \o/
-def subcommandParser(args="",nick,chan)
-	args.gsub(/\${(.*)}/) {|cmdN|
-		if not cmdN.empty? then
-			commandRunner((cmdN.strip.gsub("${","").gsub("}","") or cmdN), nick, chan)
-		end
-	}
-end
-def commandRunner(cmd,nick,chan)
-	cmd=cmd.strip
-	retFinal=""
-	retLast=""
-	rnd= ('a'..'z').to_a.shuffle[0,8].join
-	retLast=rnd
-	cmdarray = cmd.scan(/(?:[^|\\]|\\.)+/) or [cmd]
-	#func, args = cmd.lstrip().split(' ', 2)
-	cmdarray.each {|cmd|
-		cmd=cmd.lstrip
-		if cmd then
-			cmd = cmd.gsub("\\|","|")
-			func, args = cmd.split(' ', 2)
-			args = subcommandParser((args or ""),nick,chan)
-			func=func.downcase()
-			if $commands[func] then
-				if retLast==rnd then
-					retLast = ""
-					if $commands[func].is_a?(Method) then
-						retLast = $commands[func].call(args, nick, chan, args, "")
-						retLast = retLast.to_s.rstrip or ""
-					elsif $commands[func].class == Proc then
-						retLast = $commands[func].call(args, nick, chan, args, "")
-						retLast = retLast.to_s.rstrip or ""
-					elsif $commands[func].class == String then
-								retLast = $command[func].to_s.rstrip or ""
-					else
-						retLast = self.send($commands[func], args, nick, chan, args, "")
-						retLast = retLast.to_s.rstrip or ""
-					end
-				else
-					if $commands[func].is_a?(Method) then
-						retLast = $commands[func].call(args, (args or "")+retLast, chan, args, retLast)
-						retLast = retLast.to_s.rstrip or ""
-					elsif $commands[func].class == Proc then
-						retLast = $commands[func].call(args, (args or "")+retLast, chan, args, retLast)
-						retLast = retLast.to_s.rstrip or ""
-					elsif $commands[func].class == String then
-							retLast = $command[func].to_s.rstrip or ""
-					else
-						retLast = self.send($commands[func], (args or "")+retLast, nick, chan, args, retLast)
-						retLast = retLast.to_s.rstrip or ""
-					end
-				#retLast=self.send(@commands[func],(args or "")+retLast,nick,chan) or ""
-				end
-			else
-				if @cmdnotfound then
-					retLast = "No such function: '#{func}'"
-				end
-				break
-			end
-		end
-	}
-	#call func
-	#if @commands[func] then
-	#	retLast=self.send(@commands[func],args,nick,chan)
-	return retLast.rstrip if not (retLast==rnd or (retLast.to_s or "").empty?)
-	#end
-end
-def commandParser(cmd,nick,chan)
-	#job_parser = fork do
-	Thread.new do
-		begin
-			ret=commandRunner(cmd, nick, chan)
-			if ret then
-				if ret.length > 200 then
-					@bot.msg(chan,"> Output: "+putHB(ret.to_s))
-				else
-					@bot.msg(chan,"> "+ret.to_s)
-				end
-			end
-		rescue => detail
-			@bot.msg(chan,detail.message())
-		end
-		#exit
-	end
-	#Process.detach(job_parser)
-end
 def logic(raw)
 	type,nick,to,msg = @bot.msgtype(raw)
 	#sendfifos raw
@@ -192,7 +107,7 @@ def setup
 		@bot.join(chan)
 		who chan
 		sleep(0.2)
-		}
+	}
 end
 until false do
 	begin
---
+++ core/parser.rb
@@ -0,0 +1,109 @@
+# The command Parser
+# Made by vifino
+$commands ||= Hash.new()
+$variables ||= Hash.new()
+def argParser(args="",nick,chan)
+	$variables["nick"]=nick
+	$variables["chan"]=chan
+	$variables["channel"]=chan
+	$commands["let"]=->(args="",nick="",chan="",rawargs="",pipeargs=""){
+		if not args.empty? then
+			if data=args.match(/(.*?)=(.*)/) then
+				p data
+				$variables[data[1].strip] = data[2].strip
+				return "Set Variable '#{data[1]}' to '#{data[2]}'"
+			else
+				return "Missing input."
+			end
+		else
+				return "Missing input."
+		end
+	}
+	#args=args.gsub(/\$<(.*?)>/) {|var|
+	args=args.gsub(/<(.*?)>/) {|var=""|
+		if not var.empty? then
+			puts var
+			$variables[var.strip.gsub(/^</,"").gsub(/>$/,"").split(" ").first.strip]
+		end
+	}
+	args=args.gsub(/\${(.*)}/) {|cmdN|
+		if not cmdN.empty? then
+			commandRunner((cmdN.strip.gsub("${","").gsub("}","") or cmdN), nick, chan)
+		end
+	}
+end
+
+def commandRunner(cmd,nick,chan)
+	cmd=cmd.strip
+	retFinal=""
+	retLast=""
+	rnd= ('a'..'z').to_a.shuffle[0,8].join
+	retLast=rnd
+	cmdarray = cmd.scan(/(?:[^|\\]|\\.)+/) or [cmd]
+	#func, args = cmd.lstrip().split(' ', 2)
+	cmdarray.each {|cmd|
+		cmd=cmd.lstrip
+		if cmd then
+			cmd = cmd.gsub("\\|","|")
+			func, args = cmd.split(' ', 2)
+			args = argParser((args or ""),nick,chan)
+			func=func.downcase()
+			if $commands[func] then
+				if retLast==rnd then
+					retLast = ""
+					if $commands[func].is_a?(Method) then
+						retLast = $commands[func].call(args, nick, chan, args, "")
+						retLast = retLast.to_s.rstrip or ""
+					elsif $commands[func].class == Proc then
+						retLast = $commands[func].call(args, nick, chan, args, "")
+						retLast = retLast.to_s.rstrip or ""
+					elsif $commands[func].class == String then
+								retLast = $command[func].to_s.rstrip or ""
+					else
+						retLast = self.send($commands[func], args, nick, chan, args, "")
+						retLast = retLast.to_s.rstrip or ""
+					end
+				else
+					if $commands[func].is_a?(Method) then
+						retLast = $commands[func].call(args, (args or "")+retLast, chan, args, retLast)
+						retLast = retLast.to_s.rstrip or ""
+					elsif $commands[func].class == Proc then
+						retLast = $commands[func].call(args, (args or "")+retLast, chan, args, retLast)
+						retLast = retLast.to_s.rstrip or ""
+					elsif $commands[func].class == String then
+							retLast = $command[func].to_s.rstrip or ""
+					else
+						retLast = self.send($commands[func], (args or "")+retLast, nick, chan, args, retLast)
+						retLast = retLast.to_s.rstrip or ""
+					end
+				#retLast=self.send(@commands[func],(args or "")+retLast,nick,chan) or ""
+				end
+			else
+				if @cmdnotfound then
+					retLast = "No such function: '#{func}'"
+				end
+				break
+			end
+		end
+	}
+	return retLast.rstrip if not (retLast==rnd or (retLast.to_s or "").empty?)
+end
+def commandParser(cmd,nick,chan) # This is the entry point.
+	#job_parser = fork do
+	Thread.new do
+		begin
+			ret=commandRunner(cmd, nick, chan)
+			if ret then
+				if ret.length > 200 then
+					@bot.msg(chan,"> Output: "+putHB(ret.to_s))
+				else
+					@bot.msg(chan,"> "+ret.to_s)
+				end
+			end
+		rescue => detail
+			@bot.msg(chan,detail.message())
+		end
+		#exit
+	end
+	#Process.detach(job_parser)
+end
