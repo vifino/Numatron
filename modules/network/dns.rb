8c6a1a8a28a07e94f7c97f1389f362a6bda7bad4
vifino <adinator1999@gmail.com>
2014-08-05 22:13:30 +0200

    Updated dns.rb to be more user friendly

--- modules/network/dns.rb
+++ modules/network/dns.rb
@@ -6,13 +6,21 @@ def dns(addr,type="ANY")
 	rs = Dnsruby::DNS.new
 	begin
 		res = rs.getresources(addr, type)
+		p res
 		if res!=nil then
 			return (res.map {|r| r.rdata_to_string.gsub(/[[:cntrl:]]/, '') }).join("; ")
 		else
 			return "No results!"
 		end
 	rescue => e
-		return e.to_s
+		p e
+		if e.to_s == "no implicit conversion of nil into Array" then # The URL, that is given, is invalid.
+			return "Invalid URL."
+		elsif e.class==Dnsruby::NXDomain then
+			return "No entries for Domain."
+		else
+			return (e.to_s or "Error unknown.")
+		end
 	end
 end
 def rdns(addr)
@@ -20,13 +28,19 @@ def rdns(addr)
 	rs = Dnsruby::DNS.new
 	begin
 		res = rs.getnames addr
-		if res!=nil then
+		p res
+		if res.class == Hash then
 			return (res.map {|r| r.to_s }).join("; ")
 		else
 			return "No results!"
 		end
 	rescue => e
-		return e.to_s
+		p e
+		if e.to_s == "cannot interpret as address: No entries for Domain." then # The URL, that is given, is invalid.
+			return "Invalid URL or no entries for Domain."
+		else
+			return (e.to_s or "Error unknown.")
+		end
 	end
 end
 def dnsWrapper(addresses,type="ANY")
@@ -53,10 +67,18 @@ end
 def cmd_dnsa(args,nick,chan,rawargs="",pipeargs="")
 	return dnsWrapper(args,"A")
 end
+def cmd_dnsns(args,nick,chan,rawargs="",pipeargs="")
+	return dnsWrapper(args,"NS")
+end
+def cmd_dnsmx(args,nick,chan,rawargs="",pipeargs="")
+	return dnsWrapper(args,"MX")
+end
 def cmd_rdns(args,nick,chan,rawargs="",pipeargs="")
 	return rdnsWrapper(args)
 end
 $commands["dns_all"] = :cmd_dnsall
+$commands["dns_mx"] = :cmd_dnsmx
 # $commands["dns_type"] = :cmd_dnst doesnt work :/
 $commands["dns"] = :cmd_dnsa
+$commands["dns_a"] = :cmd_dnsa
 $commands["rdns"] = :cmd_rdns
