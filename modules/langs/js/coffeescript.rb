36dc36ff0c4ece58e049a9e60d43bdcd96124598
vifino <adinator1999@gmail.com>
2014-08-09 19:57:24 +0200

    Added wip coffeescript compiler

---
+++ modules/langs/js/coffeescript.rb
@@ -0,0 +1,17 @@
+# Coffeescript wrapper, requires coffeescript to be installed.
+# Install it with npm: [sudo] npm install -g coffee-script
+# Made by vifino
+def coffee2js(code)
+	rnd= ('a'..'z').to_a.shuffle[0,8].join
+	#`rm /tmp/coffee_#{rnd}`
+	`touch /tmp/coffee_#{rnd}`
+	f=File.open "/tmp/coffee_#{rnd}","w"
+	f.write code
+	f.close
+	`cat /tmp/coffee_#{rnd}|coffee -sc` # "Compiled" coffee to js will be the output.
+end
+if not `which coffee`.strip.chomp.match("coffee not found") then
+	$commands["coffeecompiler"]=->(args,nick="",chan="",rawargs="",pipeargs=""){
+		coffee2js(args.strip)
+	}
+end
