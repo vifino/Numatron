fe57a776c71ff52f2c5075b55e6b26e7504615a2
vifino <adinator1999@gmail.com>
2014-08-11 18:49:24 +0200

    Esolangs: Added XKCD style Deadfish

--- modules/langs/esolangs/deadfish.rb
+++ modules/langs/esolangs/deadfish.rb
@@ -42,3 +42,43 @@ def df_cmd(args="",nick="",chan="",rawargs="",pipeargs="")
 end
 $commands["df"] = :df_cmd
 $commands["deadfish"] = :df_cmd
+def xkcdfish(insts)
+	str="o='';i=0;"
+	insts.gsub(/./){|inst|
+		str = str+"if i>=256 then;i=0; end;if i<0 then; i=255; end;"
+		case inst
+		when "x"
+			str+="i+=1;"
+		when "d"
+			str+="i-=1;"
+		when "c"
+			str+="o+=i.chr;"
+		when "k"
+			str+="i=i*i;"
+		end
+	}
+	str+="return o;"
+	#puts str
+	begin
+		eval str
+	rescue => e
+		puts e
+		return "Error."
+	end
+end
+def dfxkcd_cmd(args="",nick="",chan="",rawargs="",pipeargs="")
+		begin
+			Timeout::timeout(2) do
+				ret=xkcdfish(args)
+				return (ret or "").delete("\r\n")
+			end
+		if ret.class == String
+		else
+			"Error: Took too long."
+		end
+	rescue SyntaxError => e
+		puts e.to_s
+		return "Error: Took too long."
+	end
+end
+$commands["xkcdfish"] = :dfxkcd_cmd
