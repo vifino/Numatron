dd6e9d040f89876d0d6736a5bbc4fead454347d6
Adrian Pistol <adinator1999@gmail.com>
2014-08-18 19:13:11 +0200

    Fixed passive.rb

--- core/passive.rb
+++ core/passive.rb
@@ -21,6 +21,8 @@ def chanProcess(chan,nick,mode=nil)
 	@passivedata["chans"][chan]["users"]||={}
 	if mode!=nil then
 		@passivedata["chans"][chan]["users"][nick]=mode.gsub(/^[hg]/i,"") # Maybe copy data over?
+		@passivedata["users"][nick]["chan"][chan]||={}
+		@passivedata["users"][nick]["chan"][chan]=mode.gsub(/^[hg]/i,"")
 		if mode.match(/^h/i) then
 			@passivedata["users"][nick]["away"]=false
 		elsif mode.match(/^g/i) then
@@ -46,15 +48,15 @@ def passive_process(raw)
 			if not @passivedata["users"].include? nick then @passivedata["users"][nick] = {} end
 			@passivedata["users"][nick]["user"] = username
 			@passivedata["users"][nick]["host"] = hostname
-			if not @passivedata["users"][nick]["chan"] then @passivedata["users"][nick]["chan"] = [] end
-			if not @passivedata["users"][nick]["chan"].include? chan then @passivedata["users"][nick]["chan"].push chan end
+			@passivedata["users"][nick]["chan"]||={}
+			i@passivedata["users"][nick]["chan"][chan]||=""
 			if type=="msg" then
 				if not @passivedata["users"][nick]["lines"] then @passivedata["users"][nick]["lines"] = 1 end
 				@passivedata["users"][nick]["lines"] += 1
 				if not @passivedata["users"][nick]["words"] then @passivedata["users"][nick]["words"] = 0 end
-				@passivedata["users"][nick]["words"] += (msg.split(" ").size) + 1
+				@passivedata["users"][nick]["words"] += msg.split(" ").size
 				if not @passivedata["users"][nick]["chars"] then @passivedata["users"][nick]["chars"] = 0 end
-				@passivedata["users"][nick]["chars"] += (msg.split("").size) + 1
+				@passivedata["users"][nick]["chars"] += msg.size
 				@passivedata["users"][nick]["avgchars"] = @passivedata["users"][nick]["chars"] / @passivedata["users"][nick]["lines"]
 				@passivedata["users"][nick]["avgwords"] = @passivedata["users"][nick]["words"] / @passivedata["users"][nick]["lines"]
 				chanProcess(chan,nick)
@@ -106,8 +108,8 @@ def passive_process(raw)
 				name = name2.strip.gsub("@","").gsub("+","")
 				puts name
 				@passivedata["users"][name]||= {}
-				@passivedata["users"][name]["chan"] ||= []
-				if not @passivedata["users"][name]["chan"].include? chan2 then @passivedata["users"][name]["chan"].push chan2 end
+				@passivedata["users"][name]["chan"] ||= {}
+				if not @passivedata["users"][name]["chan"][chan2] then @passivedata["users"][name]["chan"][chan2]="" end
 				match=name2.strip.match(/[@\+\~]/)
 				if match then
 					chanProcess(chan2,name,match[0])
@@ -127,7 +129,7 @@ def passive_process(raw)
 				chan2 = chan.strip.gsub("@","").gsub("+","")
 				if not @passivedata["users"].include? name then @passivedata["users"][name] = {} end
 				if not @passivedata["users"][name]["chan"] then @passivedata["users"][name]["chan"] = [] end
-				if not @passivedata["users"][name]["chan"].include? chan2 then @passivedata["users"][name]["chan"].push chan2 end
+				if not @passivedata["users"][name]["chan"][chan2] then @passivedata["users"][name]["chan"][chan2]="" end
 				puts chan
 				match=chan.strip.match(/[@\+\~]/)
 				if match then
@@ -165,8 +167,8 @@ def passive_process(raw)
 			acc = match[10]
 			realname = match[12].delete("^\u{0000}-\u{007F}") # Remove unicode, because it kills .to_json
 			if not @passivedata["users"].include? nick then @passivedata["users"][nick] = {} end
-			if not @passivedata["users"][nick]["chan"] then @passivedata["users"][nick]["chan"] = [] end
-			if not @passivedata["users"][nick]["chan"].include? chan then @passivedata["users"][nick]["chan"].push chan end
+			if not @passivedata["users"][nick]["chan"] then @passivedata["users"][nick]["chan"] = {} end
+			if not @passivedata["users"][nick]["chan"][chan] then @passivedata["users"][nick]["chan"][chan]="" end
 			@passivedata["users"][nick]["user"] = user
 			@passivedata["users"][nick]["host"] = host
 			@passivedata["users"][nick]["server"] = server
@@ -190,6 +192,14 @@ def passive_process(raw)
 		rescue => e
 
 		end
+	elsif type=="part" then
+		@passivedata["users"] = @passivedata["users"] or {}
+		@passivedata["users"][nick] = (@passivedata["users"][nick] or {})
+		@passivedata["users"][nick]["chan"] = @passivedata["users"][nick]["chan"] or {}
+		@passivedata["chans"][chan]=@passivedata["chans"][chan] or {}
+		@passivedata["chans"][chan]["users"]||={}
+		if @passivedata["users"][nick]["chan"][chan] then @passivedata["users"][nick]["chan"].delete(chan) end
+		if @passivedata["chans"][chan]["users"][nick] then @passivedata["chans"][chan]["users"].delete(nick) end
 	end
 end
 def passive_start
