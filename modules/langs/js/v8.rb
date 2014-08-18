8bbe534b36e56e4cebe0cace4e5c34636789181d
Adrian Pistol <adinator1999@gmail.com>
2014-08-18 19:52:22 +0200

    Another fix.

--- modules/langs/js/v8.rb
+++ modules/langs/js/v8.rb
@@ -35,7 +35,7 @@ if not @jruby then
 				if returnval!=nil then
 					txt = "\n> "+returnval.to_s.gsub("[\r\n]+","\n> ")
 				end
-				return @jsout.join(" | ").split(/[\r\n]+/).join(" | ").strip+txt.gsub("[\r\n]+"," | ")
+				return @jsout.join("\n").split(/[\r\n]+/).join(" | ").strip+txt.gsub("[\r\n]+"," | ")
 			end
 		rescue => detail
 			return detail.message
