baeb0b71aee9e3e50dbb89493229f14e1d103afa
Adrian Pistol <adinator1999@gmail.com>
2014-08-23 16:46:45 +0200

    Added ctcp ping command.

---
+++ modules/utility/ctcp.rb
@@ -0,0 +1,31 @@
+# CTCP replies and... things.
+# Made by vifino
+require 'timeout'
+def ctcpping(usr)
+	@bot.ctcp(usr,"PING POTATO")
+	isResponse=false
+	t1=Time.now
+	begin
+		Timeout::timeout(5) do
+			while true do
+				data= @bot.msgtype(@bot.lastline)
+				type = data[0]
+				nick = data[1]
+				chan = data[2]
+				msg = data[3]
+				username = data[4]
+				hostname = data[5]
+				if type=="notice" and nick=usr then#msg.match(/^\x01PING(.*)$\x01/)
+					break
+				end
+			end
+			return (Time.now-t1).to_s+" Seconds." # ....?
+		end
+	rescue => e
+		return "Timeout."
+	end
+end
+def ping(args="",nick="",chan="",rawargs="",pipeargs="")
+	ctcpping(nick)
+end
+addCommand("ping",:ping,"CTCP PINGs the user.")
