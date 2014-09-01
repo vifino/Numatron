4fdc27743752f846495c4e67e1e23679bbb8c36e
Adrian Pistol <adinator1999@gmail.com>
2014-09-01 15:59:35 +0200

    fixed a small problem

--- core/shell.rb
+++ core/shell.rb
@@ -8,21 +8,21 @@ def sh(args="")
 		#o=File.read tmpfile
 		#`rm #{tmpfile}`
 		#return o
-		return `#{args} 2>&1`
+		return `#{args.to_s} 2>&1`
 	end
 end
 def shcmd(args="",nick="",chan="",rawargs="",pipeargs=nil)
 	if isPrivileged? nick then
 		if pipeargs==nil then
-			return sh(args.to_s)
+			return sh(args)
 		else
 			rnd= ('a'..'z').to_a.shuffle[0,8].join
 			tmpfile="/tmp/#{rnd}"
 			`touch /tmp/sh_#{rnd}`
 			f=File.open "/tmp/sh_#{rnd}","w"
-			f.write pipeargs
+			f.write pipeargs.to_s
 			f.close
-			o=sh("cat /tmp/sh_#{rnd}|#{rawargs}")
+			o=sh("cat /tmp/sh_#{rnd}|#{rawargs.to_s}")
 			`rm /tmp/sh_#{rnd}`
 			return o
 		end
