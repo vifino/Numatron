c2ffc6a94b1d4288f1d6eb888c0199c4f5e74625
Adrian Pistol <adinator1999@gmail.com>
2014-09-09 01:38:59 +0200

    Updated mcping

--- modules/network/mc.rb
+++ modules/network/mc.rb
@@ -3,34 +3,36 @@ require 'socket'
 def mcping(server,port=25565)
 	port|=25565
 	begin
-		socket = TCPSocket.open(server, port)
-	rescue Timeout::Error, Errno::ECONNREFUSED, Errno::EHOSTUNREACH
+		Timeout::timeout(2) do
+			socket = TCPSocket.open(server, port)
+			socket.write([0xFE, 0x01, 0xFA].pack('CCC'))
+			socket.write(['MC|PingHost'.length].pack('n') + 'MC|PingHost'.encode('utf-16be').force_encoding('ASCII-8BIT'))
+			socket.write([7 + 2 * server.length].pack('n'))
+			socket.write([74].pack('c'))
+			socket.write([server.length].pack('n') + server.encode('utf-16be').force_encoding('ASCII-8BIT'))
+			socket.write([port].pack('N'))
+			if socket.read(1).unpack('C').first != 0xFF
+				return 'Unexpected Server response packet'
+			end
+			len = socket.read(2).unpack('n').first
+			resp = socket.read(len*2).force_encoding('utf-16be').encode('utf-8').split("\u0000")
+			socket.close
+
+			if resp.shift != "\u00A71"
+				return 'Unexpected Server response fields'
+			end
+
+			return {
+				:protocol_version => resp.shift.to_i,
+				:minecraft_version => resp.shift,
+				:motd => resp.shift,
+				:current_players => resp.shift.to_i,
+				:max_players => resp.shift.to_i
+			}
+		end
+	rescue => e
 		raise "Host unreachable."
 	end
-  socket.write([0xFE, 0x01, 0xFA].pack('CCC'))
-	socket.write(['MC|PingHost'.length].pack('n') + 'MC|PingHost'.encode('utf-16be').force_encoding('ASCII-8BIT'))
-  socket.write([7 + 2 * server.length].pack('n'))
-  socket.write([74].pack('c'))
-  socket.write([server.length].pack('n') + server.encode('utf-16be').force_encoding('ASCII-8BIT'))
-  socket.write([port].pack('N'))
-  if socket.read(1).unpack('C').first != 0xFF
-    return 'Unexpected Server response packet'
-  end
-  len = socket.read(2).unpack('n').first
-  resp = socket.read(len*2).force_encoding('utf-16be').encode('utf-8').split("\u0000")
-  socket.close
-
-  if resp.shift != "\u00A71"
-    return 'Unexpected Server response fields'
-  end
-
-  return {
-    :protocol_version => resp.shift.to_i,
-    :minecraft_version => resp.shift,
-    :motd => resp.shift,
-    :current_players => resp.shift.to_i,
-    :max_players => resp.shift.to_i
-  }
 end
 def mccmd(args="",nick="",chan="",rawargs="",pipeargs="")
 	server,port=args.split(" ",2)
