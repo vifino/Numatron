e4477639858f857db4e0688410c3cf99c0787d5e
Adrian Pistol <adinator1999@gmail.com>
2014-09-18 00:28:46 +0200

    First version of stash

--- modules/langs/esolangs/stash.rb
+++ modules/langs/esolangs/stash.rb
@@ -1,53 +1,90 @@
-# Stash ( name wip ) is a wip stack based language.
+# Stash ( name wip ) is a wip stack based language, which borrows much from Forth.
 class	Stash
 	def initialize(stack=[])
 		@stack=stack
 	end
+	def sof
+		raise("Stack underflow")
+	end
 	def pop
-		@stack.pop.to_i
+		@stack.pop || sof
 	end
 	def push(a)
-		@stack.push a
+		@stack<<a
 	end
 	def eval(insts="")
+		skip=false
 		out=[]
 		chrbuf=""
 		insts.split(" ").each{|i|
+			next if skip&&i!="FI"
 			case
-			when i=='POP'
-				pop
-			when i=='+'
-				push pop+pop
-			when '-'
-				a=pop
-				b=pop
-				push b-a
-			when i=='/'
-				a=pop
-				b=pop
-				push b/a
-			when i=='*'
-				push pop*pop
-			when i=='^'
-				a=pop
-				b=pop
-				push b**a
-			when i=='CLRBUF'
-				chrbuf+=pop.chr
-			when i=='CHRBUF'
-				chrbuf+=pop.chr
-			when i=='BUF'
-				chrbuf+=pop.to_s
-			when i=='PRINT'
-				out.push pop
-			when i=='PRNTBUF'
-				out.push chrbuf
-			when i.to_i.to_s==i # number
-				push i.to_i
+				when i=='IF'
+					skip=true if pop==0
+				when i=='FI'
+					skip=false
+				when i=='POP'
+					pop
+				when i=='+'
+					push pop+pop
+				when i=='-'
+					a=pop
+					b=pop
+					push b-a
+				when i=='='
+					push pop==pop?1:0
+				when i=='>'
+					push pop<pop?1:0
+				when i=='<'
+					push pop>pop?1:0
+				when i=='&'
+					push pop&pop?1:0
+				when i=='|'
+					push pop|pop?1:0
+				when i=='NOT'
+					push pop==0?1:0
+				when i=='NEG'
+					push -pop
+				when i=='DUP'
+					push @stack[-1]
+				when i=='SWAP'
+					begin
+						$stack[-2,2] = $stack[-2,2].reverse
+					rescue
+						sof
+					end
+				when i=='/'
+					a=pop
+					b=pop
+					push b/a
+				when i=='%'
+					a=pop
+					b=pop
+					push b%a
+				when i=='*'
+					push pop*pop
+				when i=='^'
+					a=pop
+					b=pop
+					push b**a
+				when i=='CLRBUF'
+					chrbuf+=pop.chr
+				when i=='CHRBUF'
+					chrbuf+=pop.chr
+				when i=='BUF'
+					chrbuf+=pop.to_s
+				when i=='PRINT'
+					out.push pop
+				when i=='PRNTBUF'
+					out.push chrbuf
+				when i.to_i.to_s==i.strip # number
+					push i.to_i
+				else
+					raise 'UNKNOWN WORD'
 			end
 		}
 		if out.empty?
-			out.push @stack[@stack.length] if @stack[@stack.length]
+			out.push @stack[-1] if @stack[-1]
 		end
 		return out,@stack
 	end
