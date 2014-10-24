ec18db5b4afc61e5bedf02db1264d67b09449337
Adrian Pistol <adinator1999@gmail.com>
2014-10-25 01:28:18 +0200

    Fixed few bugs

--- modules/ai/aiml.rb
+++ modules/ai/aiml.rb
@@ -1,15 +1,14 @@
 # Integration of AIML botscripts in Numatron
 
 require 'programr'
-
 aimlfiles = Dir[APP_ROOT+"/modules/ai/*.aiml"]
 
 def aiml(args="",nick="",chan="",rawargs="",pipeargs="")
-	return @aiml.get_reaction(args)
+	return $aiml.get_reaction(args)
 end
 
 if not aimlfiles.empty?
-	@aiml = ProgramR::Facade.new
-	@aiml.learn aimlfiles
+	$aiml = ProgramR::Facade.new
+	$aiml.learn aimlfiles
 	addCommand("ai",:aiml,"Query an AI")
 end
