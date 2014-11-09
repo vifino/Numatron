2324a8e8ae2aaac98c546fbe00a1ba65971600e4
Adrian Pistol <adinator1999@gmail.com>
2014-11-09 19:13:26 +0100

    Added some things.

--- modules/ai/please.rb
+++ modules/ai/please.rb
@@ -6,10 +6,13 @@ addCommand("please",->(args="",nick="",chan="",rawargs="",pipeargs="") {
 			return "PLEASE WHAT?!"
 		end
 		act = req[0]
-		rst = req[1]
-		@bot.action(chan,"#{act}s #{rst.strip}")
+		rst = req[1] || ""
+		@bot.action(chan,"#{act}s #{rst.strip}".strip.gsub(/me/i,nick).gsub(/my/,"#{nick}'s"))
 		return nil
 	else
 		"No! >:D"
 	end
 }, "You might get what you want if you ask nice.")
+#addCommand("hug" ->(args="",nick="",chan="",rawargs="",pipeargs="") {
+#
+#}, "HUGZ!")
