f71569b3140eec05fd64db67ea5c8bccffb5337a
Adrian Pistol <adinator1999@gmail.com>
2014-11-18 16:59:49 +0100

    Changed URI::excape to CGI.escape

--- core/google.rb
+++ core/google.rb
@@ -1,10 +1,9 @@
 # Google api ( WIP )
 # Made by vifino
 require "json"
-require "open-uri"
 
 class Google
 	def image(term,item=1)
-		JSON.parse(open('https://ajax.googleapis.com/ajax/services/search/images?v=1.0&start=0&q='+URI::encode(term)).read)['responseData']['results'][item]['tbUrl']
+		JSON.parse(open('https://ajax.googleapis.com/ajax/services/search/images?v=1.0&start=0&q='+CGI.encode(term)).read)['responseData']['results'][item]['tbUrl']
 	end
 end
