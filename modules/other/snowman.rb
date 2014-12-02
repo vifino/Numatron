94ffbcc77e2960acb7ebd296fcb4d3f7344fa1aa
Adrian Pistol <adinator1999@gmail.com>
2014-12-02 03:27:56 +0100

    .

--- modules/other/snowman.rb
+++ modules/other/snowman.rb
@@ -2,6 +2,6 @@
 # Made by vifino
 def doyouwanttobuild(args,nick,chan,rawargs="",pipeargs="")
 	possiblethings = ["Snowman","Meth Lab", "Gun Shop","Bomb","Hackerspace","Airplane","Paper plane","House","Supermarket","Death trap","Rocket","nuclear Rocket","Submarine","Tree House","Spaceship","Spaceshuttle","IRC network","Website Scraper","Bar","Sportsbar","Radio Station","World Destroyer"]
-	"Do you want to build a "..possiblethings.sample.."?"
+	"Do you want to build a "+possiblethings.sample+"?"
 end
 addCommand("frozen",:doyouwanttobuild,"Do you want to build a Snowman?")
