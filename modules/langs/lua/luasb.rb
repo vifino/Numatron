e331fc57927bf9992eba8c2aa453ab0532d41231
Adrian Pistol <adinator1999@gmail.com>
2015-01-19 01:10:06 +0100

    Derp 2.

--- modules/langs/lua/luasb.rb
+++ modules/langs/lua/luasb.rb
@@ -107,7 +107,7 @@ do
 				sbox[k][n]=l
 			end
 		end
-		sbo["math"]["round"] = (function(num, idp) 
+		sbox["math"]["round"] = (function(num, idp) 
 			local mult = 10^(idp or 0) 
 			return math.floor(num * mult + 0.5) / mult 
 		end)
