2e6706e044053cd54fdbe5029c8e04309caefed3
MrRatermat <MrRatermat@users.noreply.github.com>
2015-02-08 23:20:49 +0000

    Fixed missing hints on error code (hastebin get)

--- core/web.rb
+++ core/web.rb
@@ -89,7 +89,7 @@ def getHB(id="")
 		if id.length == 10 then
 			url = "http://hastebin.com/raw/"+id
 		else
-			return "Hastebin ID is wrong."
+			return "Hastebin ID is wrong. ("+id.length+")"
 		end
 		uri = URI.parse(url)
 		#Net::HTTP.get_print(uri)
@@ -120,7 +120,7 @@ def getIO(id="")
                 if id.length == 10 then
                         url = "http://pb.i0i0.me/raw/"+id
                 else
-                        return "Hastebin ID is wrong."
+                        return "Hastebin ID is wrong. ("+id.length+")"
                 end
                 uri = URI.parse(url)
                 #Net::HTTP.get_print(uri)
